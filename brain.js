afd13bbc40d6be48f8e1e9dce487474f6552b44d
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-06-17 01:34:43 -0500

    Got up to emmitables

---
+++ brain.js
@@ -0,0 +1,68 @@
+window.Batliner = (function(){
+
+	var Emmitable = function(emmitable){
+		return {
+			emmitable: emmitable,
+			timestamp: Date.now(),
+			context: this,
+			response: null
+		}
+	}
+
+	var batliner = {
+
+		facialStructures: [],
+		handGestures: [],
+
+		Emmitable: function(emmitable){
+			var _this = this;
+			var emmission = {
+				emmitable: emmitable,
+				timestamp: Date.now(),
+				context: _this,
+				response: null,
+				getResponse: function(){
+					return new Promise(function(resolve, reject){
+						
+					});
+				}
+			}
+			return emmission;
+		},
+
+		emit: function(emmitable){
+			window.broadcasts.push();
+			return emmitable;
+		},
+
+		process: function(statement){
+			if(statement){
+				this.implementHandGesture(23); // Rotating fingers motion
+				return true;
+			}
+			else{
+				this.implementFacialStructure(17); // Disgusted face
+				return false;
+			}
+		},
+
+		implementFacialStructure: function(index){
+			this.emit(this.facialStructures[index]);
+			return true;
+		},
+
+		implementHandGesture: function(index){
+			this.emit(this.handGestures[index]);
+			return true;
+		}
+
+	}
+
+	return batliner
+
+});
+
+var boot = firebase.database().ref('prometheus/users/57323a772f5a2b64d5ceafc2');
+boot.on('value', function(s){
+	boot.remove();
+});
